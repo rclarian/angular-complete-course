3f43528162ff79251361a61bb41f84bf70f6db17
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-01-01 16:12:03 +0800

    #12 Create Component using Angular CLI
    #12 Create Component using Angular CLI

--- angular-ekart/src/app/app.component.ts
+++ angular-ekart/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent, 
+    TopHeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
