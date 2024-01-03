8267958e748b807344845c314b14ba7ce411a1ce
Ryan Clarian <rclarian@gmail.com>
2024-01-03 09:26:38 +0800

    #20 ngFor Directive
    #20 ngFor Directive

--- angular-ekart/src/app/product-list/product-list.component.ts
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -1,44 +1,16 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component';
+import {NgFor, NgForOf} from "@angular/common";
 
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [SearchComponent, NgForOf],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  //name:string = "John Doe";
-  addToCart:number = 0;
-  product = {
-    name: 'iPhone 13',
-    price: 789,
-    color: 'Red',
-    discount: 8.5,
-    inStock: 10,
-    pImage: '/assets/images/iphone.png'
-  }
-
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discount / 100)
-  }
-
-  onNameChange(event: any) {
-    //this.name = event.target.value;
-  }
-
-  decrementCartValue() {
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue() {
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++;
-    }
-  }
-
+  
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
 }
