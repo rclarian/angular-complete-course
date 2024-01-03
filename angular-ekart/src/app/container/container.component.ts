c196aa682f14c015fbddf82eaea61673de2a18ca
Ryan Clarian <rclarian@gmail.com>
2024-01-03 14:59:11 +0800

    #21 Rendering List of Complex Type
    #21 Rendering List of Complex Type

---
+++ angular-ekart/src/app/container/container.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SearchComponent } from './search/search.component';
+import {NgFor, NgForOf} from "@angular/common";
+import { ProductListComponent } from './product-list/product-list.component';
+
+
+@Component({
+  selector: 'app-container',
+  standalone: true,
+  imports: [SearchComponent, NgForOf, ProductListComponent],
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+  
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
+}
